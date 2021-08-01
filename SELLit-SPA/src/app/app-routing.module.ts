c6797b8d503f5f42325088a061db3f0d146d357b
Darin Polizanov <72756517+polizanov@users.noreply.github.com>
2021-08-01 16:41:39 +0300

    implemented home route

--- SELLit-SPA/src/app/app-routing.module.ts
+++ SELLit-SPA/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  
+  {
+    path: "",
+    pathMatch: "full",
+    component: HomeComponent,
+  }, 
+  {
+    path: "**",
+    component: PageNotFoundComponent
+  }
 ];
 
 @NgModule({
--- SELLit-SPA/src/app/core/core.module.ts
+++ SELLit-SPA/src/app/core/core.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ]
 })
 export class CoreModule { }
